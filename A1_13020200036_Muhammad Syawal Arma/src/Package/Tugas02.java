2639975fdee7f537b52c6e2b309bbfe4ce2e648a
R65 <73888594+Roger-65@users.noreply.github.com>
2022-03-04 14:12:35 +0800

    A1_13020200036_Muhammad Syawal Arma
    Tugas 02

---
+++ A1_13020200036_Muhammad Syawal Arma/src/Package/Tugas02.java
@@ -0,0 +1,9 @@
+package Package;
+
+public class Tugas02 {
+	public static void main(String [] args) {
+		System.out.println("Selamat Belajar");
+		System.out.println("Belajar Bahasa Pemrograman Java");
+		System.out.println("Selesai Belajar Pemrograman Java");
+	}
+}
